1bff33bd3d78046a8acad6f9b33acfbc3c0ac71a
Vladimir Kerkhoff <v.kerkhoff@genmato.com>
2015-12-11 10:34:59 +0100

    Initial Version

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Core
+ *
+ * @package Genmato_Core
+ * @author  Vladimir Kerkhoff <support@genmato.com>
+ * @created 2015-12-11
+ * @copyright Copyright (c) 2015 Genmato BV, https://genmato.com.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Genmato_Core',
+        __DIR__
+    );
